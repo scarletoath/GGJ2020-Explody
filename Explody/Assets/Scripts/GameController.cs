0e56c8142f8a79ea448128962ff94b69882b45b9
Alex Welsh <alex.welsh42@gmail.com>
2020-02-01 17:43:49 -0600

    Adding personal test scene

---
+++ Explody/Assets/Scripts/GameController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+    PlaybackManager playbackManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        playbackManager = new PlaybackManager();
+        playbackManager.Init();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playbackManager.Update();
+    }
+
+    void OnStart()
+    {
+        // Triggered when the player starts the level.
+        // Trigger the playback manager to start recording.
+        throw new System.NotImplementedException();
+    }
+
+    void OnComplete()
+    {
+        // Triggered when the player completes the level or runs out of time
+        // Trigger the playback manager to stop recording.
+        throw new System.NotImplementedException();
+    }
+
+    void OnStartPlayback()
+    {
+        // Triggered shortly after OnComplete (x seconds? or just when the player triggeres it manually?)
+        // Trigger the playback manager to play back the recording.
+        throw new System.NotImplementedException();
+    }
+
+    void ShowMenu()
+    {
+        // Triggered by the player
+        // Pauses the game
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Explody/Assets/Scripts/PlaybackManager.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlaybackManager
+{
+    private struct PieceData
+    {
+        public Vector3 pos;
+        public Quaternion rot;
+    }
+
+    enum RecordingState {
+        WAIT,
+        RECORD,
+        REPLAY
+    }
+    private RecordingState state = RecordingState.WAIT;
+
+    public string pieceTag = "piece";
+    public ulong recordingSpeed = 10;
+    public ulong playbackSpeed = 2;
+
+    private ulong frame;
+
+    private GameObject[] pieces;
+    private Dictionary<GameObject, List<PieceData> > recordings = new Dictionary<GameObject, List<PieceData>>();
+
+    private int maxRecordings = 150;
+    private int currentReplayStep = 0;
+
+    // Start is called before the first frame update
+    public void Init()
+    {
+        frame = 0;
+        if ( pieces == null )
+        {
+            pieces = GameObject.FindGameObjectsWithTag( pieceTag );
+        }
+
+        recordingSpeed = 60 / recordingSpeed;
+        playbackSpeed = 60 / playbackSpeed;
+
+        foreach( GameObject piece in pieces )
+        {
+            recordings.Add( piece, new List<PieceData>( maxRecordings ) );
+        }
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        switch( state )
+        {
+            case RecordingState.RECORD:
+                {
+                    if ( frame % recordingSpeed == 0 ) {
+                        foreach ( GameObject piece in pieces ) {
+                            recordings[piece].Add( new PieceData {
+                                pos = piece.transform.position,
+                                rot = piece.transform.rotation
+                            } );
+                        }
+                    }
+                }
+                break;
+            case RecordingState.REPLAY:
+                {
+                    if ( frame % playbackSpeed == 0 ) {
+                        foreach ( KeyValuePair< GameObject, List< PieceData > > entry in recordings ) {
+                            GameObject piece = entry.Key;
+                            PieceData curEntry = entry.Value[ currentReplayStep ];
+
+                            piece.transform.position = curEntry.pos;
+                            piece.transform.rotation = curEntry.rot;
+                        }
+
+                    }
+                }
+                break;
+            case RecordingState.WAIT:
+            default:
+                break;
+        }
+
+        frame++;
+    }
+
+    public void StartRecording()
+    {
+        state = RecordingState.RECORD;
+    }
+
+    public void Wait()
+    {
+        state = RecordingState.WAIT;
+    }
+
+    public void StartPlayback()
+    {
+        state = RecordingState.REPLAY;
+    }
+}
