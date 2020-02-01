2b46ae28a6833c40c538807ae12948ca6e4ff5f1
Sam Fisher <sam@artandcraftentertainment.com>
2020-02-01 01:40:57 -0600

    Added basic GameInput for grab and rotate delegates. Added Grab and Rotate actions to InputManager. Added GameInput prefab. Updated SampleScene for GameInput usage. Removed deprecated placeholder files.

---
+++ Explody/Assets/Scripts/GameInput.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameInput : MonoBehaviour
+{
+
+	[Serializable] private class BoolEvent : UnityEvent <bool> {}
+	[Serializable] private class FloatEvent : UnityEvent <float> {}
+
+	[SerializeField] private string GrabAction;
+	[SerializeField] private BoolEvent OnGrab;
+
+	[Space]
+
+	[SerializeField] private string RotateAction;
+	[SerializeField] private bool RotateContinuously = true;
+	[SerializeField] private FloatEvent OnRotate;
+
+	void Update ()
+	{
+		if ( Input.GetButtonDown ( GrabAction ) )
+		{
+			OnGrab.Invoke ( true );
+			Debug.Log ( "grab true" );
+		}
+		else if ( Input.GetButtonUp ( GrabAction ) )
+		{
+			OnGrab.Invoke ( false );
+			Debug.Log ( "grab false" );
+		}
+
+		float RotateDelta = RotateContinuously || ( Input.GetButtonDown ( RotateAction ) || Input.mouseScrollDelta.y != 0 ) ? Input.GetAxis ( RotateAction ) : 0;
+		if ( RotateDelta != 0 )
+		{
+			OnRotate.Invoke ( RotateDelta );
+			Debug.Log ( $"rotate {RotateDelta}" );
+		}
+	}
+
+}
