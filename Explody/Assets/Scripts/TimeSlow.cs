267c318d65e7069e040c0a23423beb64b38136fe
destessm <destessmargiassi@gmail.com>
2020-02-01 12:31:05 -0600

    Added time slow script and prefab, changed default fixed timestep

---
+++ Explody/Assets/Scripts/TimeSlow.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeSlow : MonoBehaviour
+{
+    [SerializeField]
+    float initialSlowTime;
+
+    [SerializeField]
+    float timeToSlow;
+
+    [SerializeField]
+    float targetTimeScale;
+
+    [SerializeField]
+    float unSlowTime;
+
+    float originalTimeScale;
+    float startTime = -1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        originalTimeScale = Time.timeScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            startTime = Time.time;
+        }
+
+        if (startTime > 0)
+        {
+            // time is being slowed
+            float timeSinceStart = Time.time - startTime;
+            if (timeSinceStart < initialSlowTime)
+            {
+                // lerp to target
+                Time.timeScale = Mathf.Lerp(originalTimeScale, targetTimeScale, (timeSinceStart) / (initialSlowTime));
+            }
+            else if (Time.time - startTime < initialSlowTime + timeToSlow)
+            {
+                Time.timeScale = targetTimeScale;
+            }
+            else if (Time.time - startTime < (initialSlowTime + timeToSlow + unSlowTime))
+            {
+                // lerp to target
+                Time.timeScale = Mathf.Lerp(targetTimeScale, originalTimeScale, (timeSinceStart) / (initialSlowTime + timeToSlow + unSlowTime));
+            }
+            else
+            {
+                Time.timeScale = originalTimeScale;
+                startTime = -1;
+            }
+        }
+    }
+
+
+}
