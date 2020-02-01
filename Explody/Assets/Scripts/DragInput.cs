375335e73745c86a6f41611b8b127882d0a3e34a
destessm <destessmargiassi@gmail.com>
2020-02-01 17:37:06 -0600

    Adds dragging functionality, snap-to-location functionality, and a sample object for testing

---
+++ Explody/Assets/Scripts/DragInput.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragInput : MonoBehaviour
+{
+
+    GameObject draggedObject;
+    Vector3 dragOffset;
+
+    float rotationValue;
+    [SerializeField] float rotationSpeed;
+
+    Vector2 draggedVelocity;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (draggedObject != null)
+        {
+            // check if it has snapped to position
+            if (draggedObject.GetComponent<SnapToLocation>().GetPositionIsFixed())
+            {
+                // snapped, let go
+                //draggedObject.gameObject.GetComponent<Rigidbody2D>().simulated = true;
+                //draggedObject.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+                //draggedObject.gameObject.GetComponent<Rigidbody2D>().AddForce(draggedVelocity * 50);
+                draggedObject = null;
+                dragOffset = Vector3.zero;
+            }
+            else
+            {
+                Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                Vector2 oldPosition = draggedObject.transform.position;
+                draggedObject.transform.position = new Vector3(mousePos.x, mousePos.y, draggedObject.transform.position.z) + dragOffset;
+
+                if (rotationValue != 0)
+                {
+                    Vector3 prevRot = draggedObject.transform.rotation.eulerAngles;
+                    prevRot.z += rotationValue * rotationSpeed;
+                    draggedObject.transform.rotation = Quaternion.Euler(prevRot);
+                    rotationValue = 0;
+                }
+
+                draggedVelocity = ((Vector2)draggedObject.transform.position - oldPosition) / Time.deltaTime;
+            }
+        }
+
+    }
+
+    public void HandleGrab(bool down)
+    {
+        if (down)
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction, Mathf.Infinity);
+            if (hit)
+            {
+                if (hit.transform.CompareTag("Player") && !hit.transform.gameObject.GetComponent<SnapToLocation>().GetPositionIsFixed()) // subject to change
+                {
+                    Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                    dragOffset = hit.transform.position - mousePos;
+                    dragOffset.z = 0;
+                    draggedObject = hit.transform.gameObject;
+                    draggedObject.gameObject.GetComponent<Rigidbody2D>().simulated = false;
+                }
+            }
+        }
+        else if (draggedObject != null)
+        {
+            draggedObject.gameObject.GetComponent<Rigidbody2D>().simulated = true;
+            draggedObject.gameObject.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+            draggedObject.gameObject.GetComponent<Rigidbody2D>().AddForce(draggedVelocity * 50);
+            draggedObject = null;
+            dragOffset = Vector3.zero;
+        }
+    }
+
+    public void HandleRotation(float rotationVal)
+    {
+        rotationValue = rotationVal;
+    }
+}
---
+++ Explody/Assets/Scripts/SnapToLocation.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SnapToLocation : MonoBehaviour
+{
+    [SerializeField]
+    float tolerance;
+
+    Vector3 originalLocation;
+    Quaternion originalRotation;
+    bool lookForSnaps;
+
+    bool positionIsFixed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(lookForSnaps)
+        {
+            if(Vector3.Distance(originalLocation, transform.position) < tolerance)
+            {
+                lookForSnaps = false;
+                positionIsFixed = true;
+                transform.position = originalLocation;
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            // set snap location
+            SetSnapLocation();
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //start tracking
+            StartTracking();
+        }
+
+        if (GetPositionIsFixed())
+        {
+            GetComponent<Rigidbody2D>().simulated = false;
+        }
+
+    }
+
+    public void SetSnapLocation()
+    {
+        originalLocation = transform.position;
+        originalRotation = transform.rotation;
+    }
+
+    public void StartTracking()
+    {
+        lookForSnaps = true;
+    }
+
+    public bool GetPositionIsFixed()
+    {
+        return positionIsFixed;
+    }
+
+    public float GetScore()
+    {
+        if(positionIsFixed)
+        {
+            float rotationScore = Quaternion.Angle(transform.rotation, originalRotation);
+            return (0.5f + (rotationScore * 0.5f));
+        }
+        else
+        {
+            return 0;
+        }
+    }
+}
