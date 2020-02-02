d31a3be4f888718a3e896ee6035cc9c66a0e6969
Sam Fisher <sam@artandcraftentertainment.com>
2020-02-02 06:45:23 -0600

    Added support for defining levels and their objects, as well as randomizing from selection.

--- Explody/Assets/Scripts/GameController.cs
+++ Explody/Assets/Scripts/GameController.cs
@@ -35,12 +35,143 @@ public class GameController : MonoBehaviour
 
 	}
 
+	[System.Serializable]
+	private class LevelManager : ISerializationCallbackReceiver
+	{
+
+		private enum SelectionMode
+		{
+
+			None ,
+
+			Specified ,
+			RandomFromSelection ,
+			RandomFromAll ,
+
+		}
+
+		[System.Serializable]
+		private class Level
+		{
+
+			public SelectionMode SelectionMode = SelectionMode.RandomFromAll;
+			public int ObjectIndex = -1;
+			public int [] RandomObjectIndices;
+
+		}
+
+		[SerializeField] private List <GameObject> LevelObjects = new List <GameObject> ();
+
+		[Space]
+
+		[SerializeField] private Level PostFinalLevel = new Level { SelectionMode = SelectionMode.RandomFromAll };
+		[SerializeField] private List <Level> Levels = new List <Level> { new Level { SelectionMode = SelectionMode.RandomFromAll } };
+
+		private int [] RandomAllIndices = System.Array.Empty <int> ();
+
+		private GameObject CurrentLevelObject;
+		private int LastObjectIndex = -1;
+
+		public int CurrentLevelIndex { get; private set; } = -1;
+		public bool IsLevelActive => CurrentLevelObject != null;
+
+		public void ClearLevel ()
+		{
+			if ( CurrentLevelObject != null )
+			{
+				DestroyImmediate ( CurrentLevelObject );
+			}
+		}
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="LevelIndex"></param>
+		/// <returns>Indicates whether next level spawn succeeded.</returns>
+		public bool SpawnNextLevel () => SpawnLevel ( CurrentLevelIndex + 1 );
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="LevelIndex"></param>
+		/// <returns>Indicates whether level spawn succeeded.</returns>
+		public bool SpawnLevel ( int LevelIndex , bool AutoClear = false )
+		{
+			if ( !AutoClear && CurrentLevelObject != null )
+			{
+				Debug.LogError ( $"Level {LevelIndex} requested while current level {CurrentLevelIndex} is still active." , CurrentLevelObject );
+				return false;
+			}
+			ClearLevel ();
+
+			GameObject LevelObjectSource = null;
+			if ( LevelIndex >= 0 && LevelIndex < Levels.Count )
+			{
+				CurrentLevelIndex = LevelIndex;
+				LevelObjectSource = GetLevelObject ( Levels [ LevelIndex ] );
+			}
+			else if ( LevelIndex == Levels.Count ) // post final level mode
+			{
+				LevelObjectSource = GetLevelObject ( PostFinalLevel );
+			}
+			else // Invalid level index
+			{
+				Debug.LogError ( $"{LevelIndex} is not a valid level." );
+				return false;
+			}
+
+			if ( LevelObjectSource == null ) // Valid level index but undefined level object
+			{
+				Debug.LogError ( $"Found level {LevelIndex} but no object was defined for it or invalid SelectionMode." );
+				return false;
+			}
+
+			CurrentLevelObject = Instantiate ( LevelObjectSource );
+			return true;
+		}
+
+		private GameObject GetLevelObject ( Level Level )
+		{
+			Debug.Assert ( Level != null , "Cannot get object from null Level!" );
+			switch ( Level.SelectionMode )
+			{
+				case SelectionMode.Specified :
+					return LevelObjects [ LastObjectIndex = Level.ObjectIndex ];
+				case SelectionMode.RandomFromSelection :
+					return Level.RandomObjectIndices.Length > 0 ? LevelObjects [ LastObjectIndex = GetRandomFromArray ( Level.RandomObjectIndices ) ] : null;
+				case SelectionMode.RandomFromAll :
+					return RandomAllIndices.Length > 0 ? LevelObjects [ LastObjectIndex = GetRandomFromArray ( RandomAllIndices ) ] : null;
+			}
+			return null;
+
+			int GetRandomFromArray ( int [] Values )
+			{
+				const int MaxAttempts = 10;
+				int Value = -1;
+				int Attempts = 0;
+				while ( Attempts++ < MaxAttempts && ( Value == -1 || Value == LastObjectIndex ) )
+					Value = Values [ Random.Range ( 0 , Values.Length ) ];
+				return Value == -1 ? 0 : Value; // guaranteed safe to return 0 as array has been pre-checked for at least one element
+			}
+		}
+
+		#region ISerializationCallbackReceiver Implementation
+		public void OnBeforeSerialize () {}
+		public void OnAfterDeserialize () => RandomAllIndices = Enumerable.Range ( 0 , Levels.Count ).ToArray ();
+		#endregion
+
+	}
+
     public static GameController Instance { get; private set; }
 
 	[SerializeField] private ExplodeAtPoint Exploder;
 	[SerializeField] private TimeSlow TimeSlow;
 	[SerializeField] private StarRating StarRating;
 	[SerializeField] private ScoreDisplay ScoreDisplay;
+
+	[Space] 
+	
+	[SerializeField] private LevelManager levelManager;
 
     PlaybackManager playbackManager;
     bool bStarted = false;
@@ -67,28 +198,34 @@ public class GameController : MonoBehaviour
     // Update is called once per frame
     void Update() {
         if ( Input.GetKeyDown( KeyCode.Space ) ) {
-            if ( !bStarted ) {
-                bStarted = true;
-                OnStart();
+            if ( !bStarted && !bReadyForReplay ) { // TODO : remove readyForReplay check once states are in
+                OnStartNextLevel();
             } else if( bReadyForReplay ) {
-                OnStartPlayback();
+                //OnStartPlayback(); // TODO : Uncomment when ready to hook up
+				if ( !ScoreDisplay.IsRamping )
+					OnStartNextLevel ();
             }
         }
     }
 
-    public void OnStart()
+    public void OnStartNextLevel()
     {
         // Triggered when the player starts the level.
         // Trigger the playback manager to start recording.
-        Debug.Log( "GameController::OnStart() Called." );
+        Debug.Log( "GameController::OnStartNextLevel() Called." );
 		StarRating.gameObject.SetActive ( false );
 		ScoreDisplay.Hide ();
 		snaps.Clear ();
 		scoreTally.CreateSubScore ();
+
+		levelManager.ClearLevel ();
+		levelManager.SpawnNextLevel ();
         Exploder.ExplodeAtThisPoint ();
 		TimeSlow.StartSlowDown ();
         playbackManager.StartRecording();
-    }
+
+		bStarted = true;
+	}
 
     public void OnComplete()
     {
@@ -101,7 +238,8 @@ public class GameController : MonoBehaviour
 		StarRating.SetRating ( Random.Range ( 0.29f , 1 ) , true ); // TODO : Get score in percent
 		ScoreDisplay.ShowScore ( scoreTally.LastScore );
         bReadyForReplay = true;
-    }
+		bStarted = false;
+	}
 
     public void OnStartPlayback()
     {
@@ -111,7 +249,7 @@ public class GameController : MonoBehaviour
         playbackManager.StartPlayback();
     }
 
-    public void ShowMenu()
+	public void ShowMenu()
     {
         // Triggered by the player
         // Pauses the game
