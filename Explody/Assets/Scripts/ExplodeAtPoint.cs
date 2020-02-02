9b6a101c14c765bb1357da3e9676bc0a795f9d9b
Sam Fisher <sam@artandcraftentertainment.com>
2020-02-01 19:34:05 -0600

    Added prefabs to use ExplosionForce and ExplodeTarget.

---
+++ Explody/Assets/Scripts/ExplodeAtPoint.cs
@@ -0,0 +1,43 @@
+﻿using System.Linq;
+using UnityEngine;
+
+public class ExplodeAtPoint : MonoBehaviour
+{
+
+	[SerializeField] private Camera Camera;
+
+	[Space]
+
+	[SerializeField] private ExplosionForce ExplosionForce;
+
+	private void Awake ()
+	{
+		Debug.Assert ( ExplosionForce != null , "ExplosionForce == null" );
+	}
+
+	private void OnValidate ()
+	{
+		ExplosionForce = ExplosionForce == null ? GetComponent <ExplosionForce> () : ExplosionForce;
+	}
+
+	public void Explode ( Vector3 Position , float Radius = 0.1f )
+	{
+		var Colliders = Physics2D.OverlapCircleAll ( Position , Radius );
+		var Explodables = Colliders
+								   .Where ( c => c.attachedRigidbody != null )
+								   .Select ( c => c.GetComponent <Explodable> () )
+								   .Where ( ex => ex != null ).ToArray ();
+
+		foreach ( var Explodable in Explodables )
+		{
+			Explodable.explode ();
+		}
+		ExplosionForce.doExplosion ( Position );
+	}
+
+	public void ExplodeAtMousePosition ()
+	{
+		Explode ( Camera.ScreenToWorldPoint ( Input.mousePosition ) );
+	}
+
+}
